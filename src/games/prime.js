dbbc9b2050cc94e0064e0addd0306f489f205b1c
Denis <p_den@inbox.ru>
2019-01-16 23:30:34 +0300

    step8 brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,29 @@
+import gameEngine from '..';
+import getRandom from '../utils';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const minRandomNumber = 2;
+const maxRandomNumber = 200;
+
+const isPrime = (number) => {
+  if (number === 2) {
+    return true;
+  }
+  const maxDenom = Math.sqrt(number);
+  for (let i = 2; i <= maxDenom; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const primeTask = () => {
+  const number = getRandom(maxRandomNumber, minRandomNumber);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return { question: number, correctAnswer };
+};
+
+export default () => {
+  gameEngine(primeTask, description);
+};
